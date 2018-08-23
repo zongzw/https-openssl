c61855622a7c27e12c0a24758e6196cd30cd3159
Andrew ZONG <a.zong@f5.com>
2018-08-23 18:12:30 +0800

    cd to $cdir/gens directly for work

--- 2.self-ca-cert/run.sh
+++ 2.self-ca-cert/run.sh
@@ -4,19 +4,16 @@ cdir=`cd $(dirname $0); pwd`
 source $cdir/../settings
 
 (
-    cd $cdir
+    mkdir -p $cdir/gens
+    rm -rf $cdir/gens/*
+    cd $cdir/gens
     openssl genrsa -out ca.key 2048
     openssl req -new -x509 -days 365 -key ca.key -out ca.crt
     
     openssl genrsa -out server.key 2048
     openssl req -new -key server.key -out server.csr
-    openssl x509 -req -days 365 -in server.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt
+    openssl x509 -req -days 365 -in server.csr -CA ca.crt -CAkey ca.key -set_serial $(randint) -out server.crt
     
     cp server.crt server.key $keypair_target 
     
-    $nginx_script
-    
-    mkdir -p gens
-    rm -rf gens/*
-    mv server.key server.crt server.csr ca.crt ca.key gens
 )
